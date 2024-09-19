c2361f59c4e4c875b2337f3ce31c88bbbeef9cf6
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2024-09-19 19:42:26 -0300

    feat: Matrix Inversa

--- matrixinversa.py
+++ matrixinversa.py
@@ -1,7 +1,7 @@
-# Pedro Martins de Oliveira, 2024, Lista de Programação 1 - Algebra Linear, Prof. Hudson Bode
 import sympy as sp
+import numpy as np
 
-def matrix():
+def matrix_inversa():
     col = 4
     row = 4
     matrix = []
@@ -19,20 +19,47 @@ def matrix():
                 linha.append(var)
         matrix.append(linha)
 
-    print(matrix)
+    print("\nMatriz original:")
+    for linha in matrix:
+        print(linha)
+
+    np_matrix = np.array(matrix)
+
+    det = np.linalg.det(np_matrix)
+
+    if det == 0:
+        print("\nA matriz não é inversível (determinante igual a zero).")
+    else:
+        # Calculando a inversa
+        inversa = np.linalg.inv(np_matrix)
+
+        print("\nMatriz inversa:")
+        print(inversa)
 
     print('Pressione Enter para não digitar um valor e usar uma variável simbólica')
-    x = input('Digite o valor de U2: ')
+    x = input('Digite o valor de RE: ')
     if x == '':
-        x = sp.symbols('x') # X = U2
+        x = sp.symbols('x') # X = RE
     else:
         x = int(x)
 
-    y = input('Digite o valor de U3: ')
+    y = input('Digite o valor de P1: ')
     if y == '':
-        y = sp.symbols('y') # Y = U3
+        y = sp.symbols('y') # Y = P1
     else:
         y = int(y)
+
+    w = input('Digite o valor de P2: ')
+    if w == '':
+        w = sp.symbols('w') # W = P2
+    else:
+        w = int(w)
+
+    z = input('Digite o valor de RD: ')
+    if z == '':
+        z = sp.symbols('z') # Z = RD
+    else:
+        z = int(z)
 
     e = input('Digite o valor de E: ')
     if e == '':  
@@ -52,17 +79,15 @@ def matrix():
     else:
         l = int(l)
 
-    vector = [0,x*((e*a)/l),y*((e*a)/l),0]
+    vector = [x*((e*a)/l),y*((e*a)/l),w*((e*a)/l),z*((e*a)/l)]
 
     solution = []
-    for i in range(len(matrix)):
+    for i in range(len(inversa)):
         soma = 0
         a = []
-        for j in range(len(matrix)):
-            soma += matrix[i][j]*vector[j]
+        for j in range(len(inversa)):
+            soma += inversa[i][j]*vector[j]
         a.append(soma)
         solution.append(a)
     print(solution)
-    print("O valor de RE é :",solution[0][0],"O Valor de RD é: ",solution[3][0])
-
-matrix()
+    print(solution[0][0],solution[1][0],solution[2][0],solution[3][0])
